8c90de50a83944e1223aee9719e3913f083cd453
Neeraj Singh Tomar <neerajsinghtomar@gmail.com>
2018-03-19 14:36:38 +0530

    php page for head and images

---
+++ indexGLB.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js scheme_default">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+			<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="format-detection" content="telephone=no">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="xmlrpc.php">
+		<title>QiuckLoans &#8211; Your money. Your way</title>
+<link rel='dns-prefetch' href='http://fonts.googleapis.com/' />
+<link rel='dns-prefetch' href='http://s.w.org/' />
+<link rel="alternate" type="application/rss+xml" title="QiuckLoans &raquo; Feed" href="feed/index.html" />
+<link rel="alternate" type="application/rss+xml" title="QiuckLoans &raquo; Comments Feed" href="comments/feed/index.html" />
+<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link property="stylesheet" rel='stylesheet' id='essential-grid-plugin-settings-css'  href='wp-content/plugins/essential-grid/public/assets/css/settingse24f.css?ver=2.1.0.2' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='tp-open-sans-css'  href='http://fonts.googleapis.com/css?family=Open+Sans%3A300%2C400%2C600%2C700%2C800&amp;ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='tp-raleway-css'  href='http://fonts.googleapis.com/css?family=Raleway%3A100%2C200%2C300%2C400%2C500%2C600%2C700%2C800%2C900&amp;ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='tp-droid-serif-css'  href='http://fonts.googleapis.com/css?family=Droid+Serif%3A400%2C700&amp;ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='qis_style-css'  href='wp-content/plugins/quick-interest-slider/slider341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='rs-plugin-settings-css'  href='wp-content/plugins/revslider/public/assets/css/settings00d1.css?ver=5.4.3' type='text/css' media='all' />
+<style id='rs-plugin-settings-inline-css' type='text/css'>
+#rs-demo-id {}
+</style>
+<link property="stylesheet" rel='stylesheet' id='trx_addons-icons-css'  href='wp-content/plugins/trx_addons/css/font-icons/css/trx_addons_icons-embedded341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='swiperslider-css'  href='wp-content/plugins/trx_addons/js/swiper/swiper.min.css' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='magnific-popup-css'  href='wp-content/plugins/trx_addons/js/magnific/magnific-popup.min.css' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='trx_addons-css'  href='wp-content/plugins/trx_addons/css/trx_addons.css' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='trx_addons-animation-css'  href='wp-content/plugins/trx_addons/css/trx_addons.animation341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='woocommerce-layout-css'  href='wp-content/plugins/woocommerce/assets/css/woocommerce-layout677a.css?ver=3.0.9' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='woocommerce-smallscreen-css'  href='wp-content/plugins/woocommerce/assets/css/woocommerce-smallscreen677a.css?ver=3.0.9' type='text/css' media='only screen and (max-width: 768px)' />
+<link property="stylesheet" rel='stylesheet' id='woocommerce-general-css'  href='wp-content/plugins/woocommerce/assets/css/woocommerce677a.css?ver=3.0.9' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='js_composer_front-css'  href='wp-content/plugins/js_composer/assets/css/js_composer.min3c21.css?ver=5.1.1' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='quickloans-font-Montserrat-css'  href='wp-content/themes/quickloans/css/font-face/Montserrat/stylesheet341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='quickloans-font-google_fonts-css'  href='http://fonts.googleapis.com/css?family=Tinos%3A400%2C400italic%2C700%2C700italic&amp;subset=latin%2Clatin-ext&amp;ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='quickloans-icons-css'  href='wp-content/themes/quickloans/css/font-icons/css/fontello-embedded341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='quickloans-main-css'  href='wp-content/themes/quickloans/style.css' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='quickloans-styles-css'  href='wp-content/themes/quickloans/css/__styles341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='quickloans-colors-css'  href='wp-content/themes/quickloans/css/__colors341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='mediaelement-css'  href='wp-includes/js/mediaelement/mediaelementplayer.min51cd.css?ver=2.22.0' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='wp-mediaelement-css'  href='wp-includes/js/mediaelement/wp-mediaelement.min341d.css?ver=4.8.5' type='text/css' media='all' />
+<link property="stylesheet" rel='stylesheet' id='quickloans-responsive-css'  href='wp-content/themes/quickloans/css/responsive341d.css?ver=4.8.5' type='text/css' media='all' />
+<script type='text/javascript' src='wp-includes/js/jquery/jqueryb8ff.js?ver=1.12.4'></script>
+<script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1'></script>
+<script type='text/javascript' src='wp-content/plugins/essential-grid/public/assets/js/lightboxe24f.js?ver=2.1.0.2'></script>
+<script type='text/javascript' src='wp-content/plugins/essential-grid/public/assets/js/jquery.themepunch.tools.mine24f.js?ver=2.1.0.2'></script>
+<script type='text/javascript' src='wp-content/plugins/revslider/public/assets/js/jquery.themepunch.revolution.min00d1.js?ver=5.4.3'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+/*var wc_add_to_cart_params = {"ajax_url":"\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/?wc-ajax=%%endpoint%%","i18n_view_cart":"View cart","cart_url":"http:\/\/quickloans.ancorathemes.com\/cart\/","is_cart":"","cart_redirect_after_add":"no"};*/
+/* ]]> */
+</script>
+<script type='text/javascript' src='wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart.min677a.js?ver=3.0.9'></script>
+<script type='text/javascript' src='wp-content/plugins/js_composer/assets/js/vendors/woocommerce-add-to-cart3c21.js?ver=5.1.1'></script>
+<link rel='https://api.w.org/' href='wp-json/index.html' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.8.5" />
+<meta name="generator" content="WooCommerce 3.0.9" />
+<link rel="canonical" href="index.html" />
+<link rel='shortlink' href='index.html' />
+<link rel="alternate" type="application/json+oembed" href="wp-json/oembed/1.0/embed5ee7.json?url=http%3A%2F%2Fquickloans.ancorathemes.com%2F" />
+<link rel="alternate" type="text/xml+oembed" href="wp-json/oembed/1.0/embed838b?url=http%3A%2F%2Fquickloans.ancorathemes.com%2F&amp;format=xml" />
+<style type="text/css" media="screen">
+.qis_form {width:100%;max-width:100%;color: ;}
+.qis_form th {color:;}
+.qis, .qis__fill {height: 0.467em;background: #03a391;}
+.qis__fill {background: #fccb34;}
+.qis__handle {background: white;border: 1px solid #ffffff;width: 1.467em;height: 1.467em;position: absolute;top: -0.5em;-webkit-border-radius:%;-moz-border-radius:20%;-ms-border-radius:20%;-o-border-radius:20%;border-radius:20%;}
+.rangeslider__value-bubble { border-color: ;}
+.rangeslider__value-bubble::before { border-top-color: ;}
+.total {font-weight:bold;border-top:1px solid #FFF;margin-top:6px;text-align:left;font-size:1.467em;}
+.qis, .qis__fill {font-size:1em;width: 100%;}
+.loanoutput{text-align:left;margin-bottom:6px;}
+.qis-slideroutput {margin-top:0.7em;color:}
+.qis-slidercenter {color:#ffffff;font-size:1.467em;}
+.qis-sliderleft, .qis-sliderright {color:#ffffff;font-size:1.467em;}
+.qis-interest, .qis-repayments {color:#ffffff;font-size:1.2em;}
+.qis-total {color:#ffffff;font-size:1.2em;}
+.qis-table th {color:#FFF;background:#888;font-size:1em;}
+.qis-table td {color:#FFF;background:#465069;font-size:1em;}
+.qis-table td .primary_interest, .qis-table td .secondary_interest, .qis-table td .primary_total, .qis-table td .secondary_total {color:#ffffff;font-size:1.8em;}
+.applicationform p, .applicationform span {color:#415063;margin:4px 0;}
+.qis_form h2, .qis_form .submit {background:;color:;text-align:center;}
+h2.toggle-qis a:link, h2.toggle-qis a:hover, h2.toggle-qis a:visited, .applicationform .submit {color:;}
+.qis-error-message {color:red !important;font-weight:bold;}
+.qis-error {border:1px solid red !important;}</style><script type="text/javascript">qis__rates = [];</script>	
+			<noscript><style>.woocommerce-product-gallery{ opacity: 1 !important; }</style></noscript>
+			<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+		<meta name="generator" content="Powered by Visual Composer - drag and drop page builder for WordPress."/>
+<!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="http://quickloans.ancorathemes.com/wp-content/plugins/js_composer/assets/css/vc_lte_ie9.min.css" media="screen"><![endif]--><meta name="generator" content="Powered by Slider Revolution 5.4.3 - responsive, Mobile-Friendly Slider Plugin for WordPress with comfortable drag and drop interface." />
+<link rel="icon" href="wp-content/uploads/2017/05/map.png" sizes="32x32" />
+<link rel="icon" href="wp-content/uploads/2017/05/map.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="wp-content/uploads/2017/05/map.png" />
+<meta name="msapplication-TileImage" content="http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/map.png" />
+<script type="text/javascript">function setREVStartSize(e){
+				try{ var i=jQuery(window).width(),t=9999,r=0,n=0,l=0,f=0,s=0,h=0;					
+					if(e.responsiveLevels&&(jQuery.each(e.responsiveLevels,function(e,f){f>i&&(t=r=f,l=e),i>f&&f>r&&(r=f,n=e)}),t>r&&(l=n)),f=e.gridheight[l]||e.gridheight[0]||e.gridheight,s=e.gridwidth[l]||e.gridwidth[0]||e.gridwidth,h=i/s,h=h>1?1:h,f=Math.round(h*f),"fullscreen"==e.sliderLayout){var u=(e.c.width(),jQuery(window).height());if(void 0!=e.fullScreenOffsetContainer){var c=e.fullScreenOffsetContainer.split(",");if (c) jQuery.each(c,function(e,i){u=jQuery(i).length>0?u-jQuery(i).outerHeight(!0):u}),e.fullScreenOffset.split("%").length>1&&void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0?u-=jQuery(window).height()*parseInt(e.fullScreenOffset,0)/100:void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0&&(u-=parseInt(e.fullScreenOffset,0))}f=u}else void 0!=e.minHeight&&f<e.minHeight&&(f=e.minHeight);e.c.closest(".rev_slider_wrapper").css({height:f})					
+				}catch(d){console.log("Failure at Presize of Slider:"+d)}
+			};</script>
+<style type="text/css" data-type="vc_shortcodes-custom-css">.vc_custom_1498026632954{margin-top: 0.2rem !important;}.vc_custom_1498026662950{margin-top: -0.35rem !important;}</style><noscript><style type="text/css"> .wpb_animate_when_almost_visible { opacity: 1; }</style></noscript><style type="text/css" id="trx_addons-inline-styles-inline-css">.vc_custom_1498222100533{padding-top: 0px !important;padding-right: 0px !important;padding-bottom: 0px !important;padding-left: 1.8em !important;background-color: rgba(255,255,255,0.01) !important;*background-color: rgb(255,255,255) !important;}.vc_custom_1497624150516{background: #f7f7f7 url(wp-content/uploads/2017/05/title_bg8068.jpg?id=473) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1497623034497{padding-top: 1em !important;}.vc_custom_1498307380773{padding-left: 2em !important;background-color: #ffffff !important;}.vc_custom_1497624545908{margin-top: 1.75rem !important;}.vc_custom_1497624604478{margin-top: 1.35rem !important;}.vc_custom_1497962666936{padding-right: 3.5rem !important;}.vc_custom_1498216226410{margin-top: 0.1rem !important;}.vc_custom_1498215864922{background-color: #03aa98 !important;}.vc_custom_1498211615771{margin-top: 0.6rem !important;}.vc_custom_1493817262914{background-image: url(http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/bg-14.jpg?id=373) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1497962984548{margin-top: 0.6rem !important;}.vc_custom_1497964571839{margin-top: 0.6rem !important;}.vc_custom_1493823030301{background-image: url(http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/bg3.jpg?id=413) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1498026982258{margin-top: 1.3rem !important;}.vc_custom_1498026991887{margin-top: -0.1rem !important;}.vc_custom_1498028027471{margin-top: 0.6rem !important;}.vc_custom_1498030622757{margin-top: 0.3rem !important;}.vc_custom_1498033637157{margin-top: -2.4rem !important;}.vc_custom_1498033859016{margin-top: 1.35rem !important;}.vc_custom_1493823453687{background-color: #f2f2f1 !important;}.vc_custom_1498034086687{margin-top: -0.7rem !important;}.vc_custom_1498036839396{margin-top: 0.75rem !important;}.vc_custom_1493806304198{background-image: url(http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/bg4.jpg?id=353) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1497972836092{margin-top: -1.6rem !important;}.vc_custom_1498470037129{margin-top: 2.25rem !important;}.vc_custom_1498037735259{margin-top: 1.2rem !important;}.vc_custom_1498037768026{margin-top: 1.1rem !important;}.vc_custom_1498116693896{margin-top: -0.7rem !important;}.vc_custom_1498045911214{margin-top: -0.5rem !important;}.vc_custom_1497973545573{margin-top: 1.2rem !important;}.vc_custom_1498045911214{margin-top: -0.5rem !important;}.vc_custom_1497973545573{margin-top: 1.2rem !important;}.vc_custom_1498045911214{margin-top: -0.5rem !important;}.vc_custom_1497973545573{margin-top: 1.2rem !important;}.vc_custom_1493888829418{background-image: url(http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/app_bg.jpg?id=431) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1498120887658{margin-top: -1rem !important;}.vc_custom_1498121125585{margin-top: -0.6rem !important;}.vc_custom_1498122310832{margin-top: -0.3rem !important;}.vc_custom_1497974205870{background-image: url(http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/bg5.jpg?id=361) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1497974255851{margin-top: 1rem !important;}.vc_custom_1497974262174{margin-top: 1rem !important;}.vc_custom_1493889886214{background-image: url(http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/bg-20.jpg?id=437) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1498199014569{margin-top: 0.9rem !important;}.vc_custom_1498199241314{margin-top: 1.3rem !important;}.vc_custom_1498468914316{background-color: #f2f2f1 !important;}.vc_custom_1493889988900{background-color: #f9f9f8 !important;}.vc_custom_1493889977392{background-color: #f2f2f1 !important;}.vc_custom_1498199093392{margin-top: 1.65rem !important;}.vc_custom_1498468887060{padding-right: 4rem !important;padding-left: 4rem !important;}.vc_custom_1498199200145{margin-top: 1.3rem !important;}.vc_custom_1498199093392{margin-top: 1.65rem !important;}.vc_custom_1498468850451{padding-right: 4rem !important;padding-left: 4rem !important;}.vc_custom_1498199200145{margin-top: 1.3rem !important;}.vc_custom_1498199093392{margin-top: 1.65rem !important;}.vc_custom_1498468857041{padding-right: 4rem !important;padding-left: 4rem !important;}.vc_custom_1498199200145{margin-top: 1.3rem !important;}.vc_custom_1493734918243{background-image: url(http://quickloans.ancorathemes.com/wp-content/uploads/2017/05/bg_footer.jpg?id=239) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}.vc_custom_1493734874165{background-color: #1f2529 !important;}.vc_custom_1497626832139{margin-top: 0.4rem !important;}.vc_custom_1497627900450{margin-top: 1.95rem !important;}.vc_custom_1497626591285{margin-top: 0.8rem !important;}.vc_custom_1497626506244{margin-top: 1.5rem !important;}</style><style>" + htmlDivCss + "</style><style>" + htmlDivCss + "</style><style>' + htmlDivCss + '</style></head>
